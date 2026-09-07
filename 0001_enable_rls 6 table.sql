b6f0353886bab0093a71694205e60f62be0f3f49
batoolfizza332-cpu <batoolfizza332@gmail.com>
2026-09-07 21:20:04 +0100

    Add files via upload

---
+++ 0001_enable_rls 6 table.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "public"."articles" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."categories" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."ingredients" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."product_ingredients" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."product_related_products" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."products" ENABLE ROW LEVEL SECURITY;
